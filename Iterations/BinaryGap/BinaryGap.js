0542ae6a8e314d49c3c8bf4fac1c72f304bb1ec7
przemek <nowicki.przemek@gmail.com>
2018-06-19 10:02:09 +0200

    BinaryGap solution

--- Iterations/BinaryGap/BinaryGap.js
+++ Iterations/BinaryGap/BinaryGap.js
@@ -1,3 +1,17 @@
 function solution(N) {
+    const binary = parseInt(N, 10);
+    const binaryRep = binary.toString(2).split('');
+    let zerosCnt = 0;
+    let binaryGap = 0;
+    for(let i=0; i<binaryRep.length; i++) {
+        if(binaryRep[i] === '1') {
+            binaryGap = zerosCnt > binaryGap ? zerosCnt : binaryGap;
+            zerosCnt = 0;
+        } else {
+            zerosCnt++;
+        }
+    }
+    return binaryGap;
 }
 
+console.log(solution(1041));
